2e1b76af487d084eddfdd070917206244bb3e39e
Carlos Henriquez <carlos.henriquez@consultor.wom.cl>
2020-02-10 16:16:42 -0300

    isconstructing logic and header modifiers

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,60 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const HeaderElement = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+  align-items:center;
+  position:relative;
+  margin-bottom:20px;
+  
+  h1{
+      position:relative;
+      margin:0px;
+      padding:0px 10px;
+
+      &:before{
+      content:'';
+      position:absolute;
+      width:100%;
+      height:4px;
+      background:white;
+      top:0px;
+      left:0px;
+      }
+      &:after{
+          content:'';
+          position:absolute;
+          width:100%;
+          height:4px;
+          background:white;
+          bottom:0px;
+          left:0px;
+      }
+  }
+`
+const Content = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+  align-items:center;
+  position:relative;
+  margin-bottom:20px;
+  p{
+    text-align:center;
+  }
+`
 
 const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
-            <header className="major">
-                <h1>Hola, mi nombre es tu vieja en cuatro</h1>
-            </header>
-            <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
+            <HeaderElement>
+                <h1>Bee-Drone</h1>
+            </HeaderElement>
+            <Content>
+                <p>TEXTO PULENTO DE RELLENO</p>
+            </Content>
         </div>
     </section>
 )
